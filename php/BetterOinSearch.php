4abba9c80ab3af0acf5098472f366982af360553
Andrew Ry <RyAndrew@users.noreply.github.com>
2021-09-20 23:35:51 -0500

    add updating data via api

--- php/BetterOinSearch.php
+++ php/BetterOinSearch.php
@@ -1,6 +1,6 @@
 <?php
 
-class BetterOinSearch extends AgileBaseController {
+class BetterOinSearch {
 
 	function getCsvHeaders()
 	{
@@ -39,4 +39,76 @@ class BetterOinSearch extends AgileBaseController {
 
 	}
 
+	function updateOinData(){
+
+		require_once('key.php');
+
+		$curlSession = curl_init();
+
+		curl_setopt_array($curlSession, [
+			CURLOPT_URL => $url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_FOLLOWLOCATION => false,
+			CURLOPT_HTTPHEADER => ["Authorization: SSWS ".$key]
+		]);
+
+		$server_output = curl_exec($curlSession);
+		$curl_error = curl_error($curlSession);
+
+		if($server_output === false){
+			die("Failed to curl to '".$url."' ".$curl_error);
+		}
+
+		$responseCode = curl_getinfo($curlSession, CURLINFO_RESPONSE_CODE);
+		if($responseCode !== 200){
+			die("Curl Response Code not 200! Code returned: {$responseCode}<BR>\r\n<BR>\r\n{$server_output}");
+		}
+
+		curl_close($curlSession);
+
+		$oinLines = explode("\n",$server_output);
+
+		array_shift($oinLines);//remove first header line
+		array_pop($oinLines);//remove last blank line
+
+		foreach($oinLines as $line){
+
+			$line = str_getcsv($line);
+
+			foreach($line as &$col){
+				$col = trim($col);
+				$col = mb_convert_encoding($col, 'UTF-8', mb_detect_encoding($col, 'UTF-8, ISO-8859-1', true));
+			}
+			$data[] = $line;
+		}
+
+		if(false === $dataJson = json_encode($data, JSON_THROW_ON_ERROR)){
+			die('failed to json encode!');
+		}
+
+		$rootDir = $_SERVER['DOCUMENT_ROOT'].'/BetterOinSearch/';
+		$date = date('Y-m-d_g-i-s');
+		@rename($rootDir.'oin.json',"{$rootDir}oin-{$date}.json");
+		file_put_contents($rootDir.'oin.json',$dataJson);
+
+		echo "oin.json updated! {$date}";
+	}
+
+	function route(){
+
+		if(FALSE === $lastSlash = strrpos($_SERVER['REQUEST_URI'], '/')){
+			die('invalid route');
+		}
+		$method = substr($_SERVER['REQUEST_URI'],$lastSlash+1);
+
+		if(FALSE === is_callable([$this, $method])){
+			die('invalid route');
+		}
+		$this->$method();
+
+	}
+
 }
+
+$inst = new BetterOinSearch();
+$inst->route();
