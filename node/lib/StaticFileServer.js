36d6cf48bf1b732e8f99d368cca604a95cdbb2d4
Andrew Ry <RyAndrew@users.noreply.github.com>
2021-10-02 17:29:25 -0500

    static webRoot

--- node/lib/StaticFileServer.js
+++ node/lib/StaticFileServer.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default class StaticFileServer {
-	webRoot = ''
+	static webRoot = ''
 
 	// maps file extention to MIME types
 	static mimeTypes = {
